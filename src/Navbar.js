5546439d0fe60ff269ea49b0c5028cc7cb400824
Haochen Gou <hgou@ualberta.ca>
2022-05-19 09:53:11 -0600

    Update

---
+++ src/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./css/Navbar.css";
+
+const Navbar =()=>{
+    return(
+        <div>
+        <h1>Lavalamp CSS Menu</h1>
+
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Blog</a>
+            <a href="#">Portefolio</a>
+            <a href="#">Contact</a>
+            <div class="animation start-home"></div>
+        </nav>
+        </div>
+    );
+};
+
+export default Navbar;
